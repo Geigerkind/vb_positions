1c25762884ff2b1589f368e33e17b31a247938ed
Tom Dymel <tom@dymel.eu>
2022-12-16 22:02:53 +0100

    Increase shape size

--- frontend/src/app/field/shapes/half-circle.ts
+++ frontend/src/app/field/shapes/half-circle.ts
@@ -4,7 +4,7 @@ import { ShapeType } from "../value/shape-type";
 import { ShapeDto } from "../dto/shape-dto";
 
 export class HalfCircle extends Shape {
-  private static RELATIVE_RADIUS: number = 0.018;
+  private static RELATIVE_RADIUS: number = 0.018 * 2;
   public readonly shape_type: ShapeType = ShapeType.HalfCircle;
 
   constructor(actor: Actor, context: CanvasRenderingContext2D, x: number, y: number) {
