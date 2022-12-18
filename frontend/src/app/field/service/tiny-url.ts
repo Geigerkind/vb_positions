32d983442a8edc2291f3864e73b2ad27d4cf92fd
Tom Dymel <tom@dymel.eu>
2022-12-18 11:24:42 +0100

    Try another url shortener

--- frontend/src/app/field/service/tiny-url.ts
+++ frontend/src/app/field/service/tiny-url.ts
@@ -1,27 +1,16 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { map, Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class TinyUrl {
-  private headers = new HttpHeaders({ "Content-Type": "application/json" });
-
   constructor(private http: HttpClient) {}
 
   shorten(url: string): Observable<string> {
-    return this.http
-      .post(
-        "https://abbrefy.xyz/api/v1/url/abbrefy",
-        {
-          url: url,
-        },
-        {
-          headers: this.headers,
-          responseType: "json",
-        }
-      )
-      .pipe(map((data: any) => data));
+    return this.http.get("https://cdpt.in/shorten?url=" + encodeURI(url), {
+      responseType: "text",
+    });
   }
 }
