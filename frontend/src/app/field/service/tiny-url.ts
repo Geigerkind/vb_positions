d2af4b9cf7eb0da939685505f622ba7dcbd6e472
Tom Dymel <tom@dymel.eu>
2022-12-18 20:10:27 +0100

    Fix export and import using tinyurl

--- frontend/src/app/field/service/tiny-url.ts
+++ frontend/src/app/field/service/tiny-url.ts
@@ -1,16 +1,41 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { map, Observable, of } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class TinyUrl {
+  private headers = new HttpHeaders({
+    "Content-Type": "application/json",
+    "Authorization": "Bearer GMTjuO1F77wSxW1mLH19TTQr2kHLLZVAohLge90rnA8v0StURQVoYuAOKsmc",
+  });
+
   constructor(private http: HttpClient) {}
 
   shorten(url: string): Observable<string> {
-    return this.http.get("https://cdpt.in/shorten?url=" + encodeURIComponent(url), {
-      responseType: "text",
-    });
+    return this.http
+      .post(
+        "https://api.tinyurl.com/create",
+        {
+          url: url,
+          domain: "tinyurl.com",
+        },
+        {
+          headers: this.headers,
+          responseType: "json",
+        }
+      )
+      .pipe(map((data: any) => data.data.tiny_url));
+  }
+
+  resolve(tinyurl: string): Observable<string> {
+    if (!tinyurl.includes("tinyurl.com")) {
+      return of();
+    }
+    const alias = tinyurl.split("tinyurl.com/")[1];
+    return this.http
+      .get(`https://api.tinyurl.com/alias/tinyurl.com/${alias}`, { headers: this.headers, responseType: "json" })
+      .pipe(map((result: any) => result.data.url));
   }
 }
